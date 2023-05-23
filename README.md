459a0f3a9f700cf244a63d0e95ea259c2ebd3107
pedrozc90 <pedrozc90@gmail.com>
2023-05-23 13:05:19 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ScreenShots
+
+World of warcraft addont to take screenshots automatically when some events occure.
---
+++ settings.lua
@@ -0,0 +1,11 @@
+local _, ns = ...
+local ScreenShots = ns.ScreenShots
+
+ScreenShots.Config = {
+    ["Enable"] = true,                              -- enables plugin.
+    ["Achievements"] = true,                        -- enables screenshots of earned achievements.
+    ["BossKills"] = false,                          -- enables screenshots of successful boss encounters.
+    ["ChallendeMode"] = false,                      -- enables screenshots of successful challenge modes.
+    ["LevelUp"] = false,                            -- enables screenshots when player level up.
+    ["Messages"] = false                            -- enables debug messages.
+}
